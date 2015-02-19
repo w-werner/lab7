d4ad0b4c1a67756907b9a9a3ef78b6f00b21cb45
William Werner <wwerner@ucsd.edu>
2015-02-19 19:26:55 +0000

    Databases lab

--- routes/project.js
+++ routes/project.js
@@ -3,6 +3,11 @@ var models = require('../models');
 exports.projectInfo = function(req, res) { 
   var projectID = req.params.id;
 
+    models.Project
+      .find({"_id": projectID})
+      .sort('-date')
+      .exec(afterQuery);
+
   // query for the specific project and
   // call the following callback
 
@@ -16,6 +21,23 @@ exports.addProject = function(req, res) {
   var form_data = req.body;
   console.log(form_data);
 
+  var newProject = new models.Project({
+      "title": form_data['project_title'],
+      "date": new Date(form_data['date']),
+      "summary": form_data['summary'],
+      "image": form_data['image_url']
+  });
+
+  newProject.save(afterSaving);
+
+  function afterSaving(err){
+    if(err) {
+      console.log(err);
+      res.send(500);
+    }
+    res.redirect('/');
+  }
+
   // make a new Project and save it to the DB
   // YOU MUST send an OK response w/ res.send();
 }
@@ -23,6 +45,14 @@ exports.addProject = function(req, res) {
 exports.deleteProject = function(req, res) {
   var projectID = req.params.id;
 
+  models.Project
+        .find({"_id": projectID})
+        .remove()
+        .exec(afterDelete);
+
+  function afterDelete(err){
+    if(err){console.log(err);res.send();}
+  }
   // find the project and remove it
   // YOU MUST send an OK response w/ res.send();
 }
